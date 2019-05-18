4c947843cd34559bdf342551bd6759b02e860eb3
HQM <hakim@femaledaily.com>
2019-05-18 18:40:22 +0700

    user detail

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,14 @@ router.get('/', function(req, res, next) {
   res.render('users/list', {title: 'users list', users: user_list})
 });
 
+/* GET users listing and generate table. */
 router.get('/list', function(req, res, next){
   res.render('users/table', {title: 'users list', users: user_list })
+});
+
+/* GET users detail base on their's username . */
+router.get('/detail/:username', function(req, res, next){
+  u = user_list.find(user => user.name === req.params.username )
+  res.render('users/detail', {title: 'Detail user', user: u})
 });
 module.exports = router;
