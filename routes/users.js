c1618c2338cb3ed7b89c4c60749760c909c17a47
HQM <hakim@femaledaily.com>
2019-05-18 16:47:00 +0700

    - add navbar - add users list template - remove body padding

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users/list', {title: 'users list', users: [
+    {name: 'HQM', address: 'Jakarta'},
+    {name: 'Arnest', address: 'Wates'},
+  ]})
 });
 
 module.exports = router;
