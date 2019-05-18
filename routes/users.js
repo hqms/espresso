117db08deff1135a585a08a9db9bdc4b36e84ee9
HQM <hakim@femaledaily.com>
2019-05-18 17:22:46 +0700

    add /users/list

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var user_list = [
+  {name: 'HQM', address: 'Jakarta'},
+  {name: 'Arnest', address: 'Wates'},
+]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/list', {title: 'users list', users: [
-    {name: 'HQM', address: 'Jakarta'},
-    {name: 'Arnest', address: 'Wates'},
-  ]})
+  res.render('users/list', {title: 'users list', users: user_list})
 });
 
+router.get('/list', function(req, res, next){
+  res.render('users/table', {title: 'users list', users: user_list })
+});
 module.exports = router;
